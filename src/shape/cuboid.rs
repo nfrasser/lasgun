d51fddae09d765ab63819cd97f71b31d702d68d3
Nick Frasser <nfrasser@gmail.com>
2018-06-16 17:00:26 -0400

    Initial Box implementation
    Known here as a Cuboid to avoid conflicts with the Box type defined in the prelude

---
+++ src/shape/cuboid.rs
@@ -0,0 +1,27 @@
+use space::*;
+use shape::Shape;
+use shape::Intersection;
+
+/**
+    aka "Box", aka "Rectangular prism"
+*/
+pub struct Cuboid {
+    pub bounds: (Point, Point)
+}
+
+impl Cuboid {
+    pub fn new(bound0: Point, bound1: Point) -> Cuboid {
+        Cuboid { bounds: (bound0, bound1) }
+    }
+
+    pub fn cube(origin: Point, dim: f64) -> Cuboid {
+        Cuboid { bounds: (origin, origin + Vector::repeat(dim)) }
+    }
+}
+
+impl Shape for Cuboid {
+    fn intersect(&self, e: &Point, d: &Direction) -> Intersection {
+        let (b0, b1) = (&self.bounds.0, &self.bounds.1);
+        Intersection::none()
+    }
+}
