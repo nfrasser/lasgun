9dcb0410b49d6f2c1bf8d353e331a6bab2741562
Nick Frasser <nfrasser@gmail.com>
2018-06-17 01:06:43 -0400

    Scene definition update
    aggregate -> content, added dimensions and fov

--- src/scene.rs
+++ src/scene.rs
@@ -5,11 +5,14 @@ use primitive::Primitive;
 use shape::Intersection;
 
 pub struct Scene {
-    pub aggregate: Box<Primitive>,
+    pub content: Box<Primitive>,
+    pub dimensions: (u16, u16), // width/height of generated image
 
     pub eye: Point,
     pub view: Vector,
     pub up: Vector,
+
+    pub fov: f64, // field of view
 
     pub ambient: Color, // ambient lighting
     pub lights: Vec<Box<Light>> // point-light sources in the scene
@@ -17,8 +20,8 @@ pub struct Scene {
 
 impl Scene {
     // Find the intersection for the ray defined by the given eye/direction If no intersection is
-    // found, returns an intersetion at infinity with the root aggregate primitive.
+    // found, returns an intersetion at infinity with the root content primitive.
     pub fn intersect(&self, e: &Point, d: &Direction) -> (Intersection, &Primitive) {
-        self.aggregate.intersect(e, d)
+        self.content.intersect(e, d)
     }
 }
