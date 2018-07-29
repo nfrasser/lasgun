d29d57d34eeaa65dabe1b41e152c6a1fc3975551
Nick Frasser <nfrasser@gmail.com>
2018-07-28 22:09:59 -0400

    Add Normal face_forward method

--- src/space/normal.rs
+++ src/space/normal.rs
@@ -1,4 +1,4 @@
-use na::{ Scalar, Vector3 };
+use na::{ Real, Scalar, Vector3 };
 
 /**
 Normal vector representation.
@@ -6,6 +6,7 @@ Normal vector representation.
 For situations where we want the vector to be treated as a normal
 e.g., when doing transformations to maintain normal behaviour
 */
+#[derive(Debug, Copy, Clone)]
 pub struct Normal3<N: Scalar>(pub Vector3<N>);
 
 impl<N: Scalar> Normal3<N> {
@@ -23,4 +24,12 @@ impl<N: Scalar> Normal3<N> {
     Get the value of the underlying vector
     */
     pub fn unwrap(&self) -> Vector3<N> { self.0 }
+
+}
+
+impl<N: Scalar + Real> Normal3<N> {
+    /// Ensure the normal is facing toward the given d vector
+    pub fn face_forward(&self, d: &Vector3<N>) -> Normal3<N> {
+        Normal3(if self.0.dot(d).is_sign_negative() { -self.0 } else { self.0 })
+    }
 }
