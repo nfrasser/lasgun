4bf6d946c9bcbfd9405599322928c074500fddba
Nick Frasser <nfrasser@gmail.com>
2019-06-09 17:50:58 -0400

    Add metal examples

--- src/examples/simplecows.rs
+++ src/examples/simplecows.rs
@@ -23,7 +23,7 @@ fn simplecows() -> Scene {
     scene.add_point_light([200.0, 202.0, 430.0], [0.8, 0.8, 0.8], [1.0, 0.0, 0.0]);
 
     // Materials
-    let stone = scene.add_plastic_material([0.8, 0.7, 0.7], [0.0, 0.0, 0.0], 0.0);
+    let stone = scene.add_metal_material([0.0, 0.0, 0.0], [0.7, 0.7, 0.7], 0.5, 0.5);
     let grass = scene.add_plastic_material([0.1, 0.7, 0.1], [0.0, 0.0, 0.0], 0.0);
     let hide = scene.add_plastic_material([0.84, 0.6, 0.53], [0.3, 0.3, 0.3], 0.1);
 
